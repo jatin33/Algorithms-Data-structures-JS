7e3d1a97c94facd735c80231d266c0c491e82a87
Jatin Panjwani <jatinpanjwani12@gmail.com>
2018-12-21 00:04:57 +0530

    Update Seek and Destroy.js

--- Intermediate Algorithm challenges/Seek and Destroy.js
+++ Intermediate Algorithm challenges/Seek and Destroy.js
@@ -4,29 +4,28 @@
 // Remove all elements from the initial array that are of the same value as these arguments.
 
 function destroyer(arr) {
-  // Remove all the values
-  let args = Array.from(arguments);
-  let arrToDestroy = args[0];
-
-  for(let i = 1; i < args.length; i++){
-    arrToDestroy.splice(arrToDestroy.indexOf(args[i]),1);
-  }
-  return arrToDestroy;
+  var args = Array.from(arguments).slice(1);
+  console.log(args); // gives us array from index 1 to end of arguments array
+  console.log(arr); // gives us first element of argument of array
+  return arr.reduce((acc,curr)=>{
+    if(!args.includes(curr)) acc.push(curr)
+    return acc;
+  },[]);
 }
 
 // Test cases
 //=================
 
-//  Failed
-//  destroyer([1, 2, 3, 1, 2, 3], 2, 3) should return [1, 1] but returns [1, 1, 2, 3] so try using Regular expression.
+//  Passed
+//  destroyer([1, 2, 3, 1, 2, 3], 2, 3) should return [1, 1].
 
-//  Failed
+//  Passed
 //  destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3) should return [1, 5, 1].
 
-//  Failed
+//  Passed
 //  destroyer([3, 5, 1, 2, 2], 2, 3, 5) should return [1].
 
-//  Failed
+//  Passed
 //  destroyer([2, 3, 2, 3], 2, 3) should return [].
 
 //  Passed
