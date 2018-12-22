2bf1ff4a98c99b2cb02f0cfcaef5c68b74b41148
Jatin Panjwani <jatinpanjwani12@gmail.com>
2018-12-22 12:22:58 +0530

    All test cases solved

--- Intermediate Algorithm challenges/Wherefore art thou.js
+++ Intermediate Algorithm challenges/Wherefore art thou.js
@@ -1,9 +1,25 @@
 function whatIsInAName(collection, source) {
   // What's in a name?
   var arr = [];
+  //console.log(collection);
+  //console.log(source);
   // Only change code below this line
-  
-  
+    // arr = collection.filter((obj)=>{
+    //   return obj.hasOwnProperty(source);
+    // });
+    //console.log(Object.getOwnPropertyNames(source)); 
+    //console.log(Object.entries(source));
+    let srcKeys = Object.keys(source);
+    console.log(srcKeys);
+    arr = collection.filter((obj)=>{
+      for(let i = 0; i < srcKeys.length; i++)
+      {
+        if(!obj.hasOwnProperty(srcKeys[i]) || obj[srcKeys[i]]!==source[srcKeys[i]]){
+          return false;
+        }
+      }
+      return true;
+    });
   // Only change code above this line
   return arr;
 }
