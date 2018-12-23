e808cde77e78ccb86c19c14516e52d342bb36dff
Jatin Panjwani <jatinpanjwani12@gmail.com>
2018-12-24 00:35:49 +0530

    All test cases passed

--- Intermediate Algorithm challenges/dnaPairing.js
+++ Intermediate Algorithm challenges/dnaPairing.js
@@ -25,3 +25,9 @@ function pairElement(str) {
 
 pairElement("GCG");
 
+//========================Test Cases==================
+// pairElement("ATCGA") should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
+// Passed
+// pairElement("TTGAG") should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
+// Passed
+// pairElement("CTCTA") should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].
