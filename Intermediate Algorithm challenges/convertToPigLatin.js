f83010f362f8b55b9657fb30fdef90a84ad08367
Jatin Panjwani <jatinpanjwani12@gmail.com>
2018-12-23 15:22:08 +0530

    All Testcases passed

--- Intermediate Algorithm challenges/convertToPigLatin.js
+++ Intermediate Algorithm challenges/convertToPigLatin.js
@@ -6,24 +6,44 @@
 
 // Input strings are guaranteed to be English words in all lowercase.
 
+const consonantCluster = /[^aeiou]+/;//one consonant or sequence of consonants
+const vowelRegex = /[aeiou]/;
+
 function translatePigLatin(str) {
-  console.log(indexOfFirstVowel(str)); 
-  return str;
+  if(startsWithVowel(str)){
+    return str + 'way';
+  }else if(hasNoVowel(str)){
+    return str + 'ay';
+  }else{
+    let leadingWord = str.substring(indexOfFirstVowel(str));
+    let matches = str.match(/[^aeiou]+/);
+    return leadingWord + matches[0] + 'ay';
+  }  
 }
 
-translatePigLatin("bcde");
+translatePigLatin("california");
 
 function indexOfFirstVowel(word){
-  let matches = word.match(/[^aeiou]+/);
+  let matches = word.match(consonantCluster);
   return matches[0].length;
 }
 
 function startsWithVowel(word){
   // determines if word starts with vowel
-  return word.search(/[aeiou]/) === 0 ? true : false;
+  return word.search(vowelRegex) === 0 ? true : false;
 }
 
 function hasNoVowel(word){
   // checks if word has no vowel at all
-  return word.search(/[aeiou]/) === -1 ? true:false;
+  return word.search(vowelRegex) === -1 ? true:false;
 }
+
+//===================Testcases=======================
+
+//translatePigLatin("california") should return "aliforniacay".
+// translatePigLatin("paragraphs") should return "aragraphspay".
+// translatePigLatin("glove") should return "oveglay".
+// translatePigLatin("algorithm") should return "algorithmway".
+// translatePigLatin("eight") should return "eightway".
+// Should handle words where the first vowel comes in the end of the word.
+// Should handle words without vowels.
