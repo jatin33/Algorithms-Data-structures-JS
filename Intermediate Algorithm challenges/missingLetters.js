54a423e3dbe9e27a5ed2962877a903a17a14820b
Jatin Panjwani <jatinpanjwani12@gmail.com>
2018-12-24 11:23:41 +0530

    Create missingLetters.js

---
+++ Intermediate Algorithm challenges/missingLetters.js
@@ -0,0 +1,21 @@
+// Problem statement : Find the missing letter in the passed letter range and return it.
+// If all letters are present in the range, return undefined.
+
+function fearNotLetter(str) {
+  let missedLetter = '';
+  let noMissedLetterFreq = 0;
+  for(let i = 0; i < str.length - 1; i++){
+    let consDiff = Math.abs(str.charCodeAt(i)-str.charCodeAt(i+1));
+    if(consDiff!==1){
+      missedLetter = String.fromCharCode(str.charCodeAt(i)+1);
+    }else{
+      noMissedLetterFreq++;
+    }
+  }
+  if(noMissedLetterFreq === (str.length-1)){
+    return undefined;
+  }else{
+    return missedLetter;
+  }
+}
+fearNotLetter("bcdf");
