390f130715cf49b589b3cc605da64452274327fc
Jatin Panjwani <jatinpanjwani12@gmail.com>
2018-12-25 19:21:54 +0530

    All test cases passed

--- Intermediate Algorithm challenges/sortedUnion.js
+++ Intermediate Algorithm challenges/sortedUnion.js
@@ -1,4 +1,4 @@
-//Problem statement:Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+//Problem statement: Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
 
 // In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
 
@@ -8,11 +8,12 @@
 
 function uniteUnique(arr) {
   let united = [];
-
-  return arr;
+  united = united.concat(arguments[0]);
+  for(let i = 0; i < arguments.length-1; i++) {
+    united = united.concat(removeDuplicates(united,arguments[i+1]));
+  }
+  return united;
 }
-
-removeDuplicates([1,3,2],[5,2,1,4]);
 
 function removeDuplicates(src,consistsDups){
   let unique = [];
@@ -28,8 +29,10 @@ function removeDuplicates(src,consistsDups){
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
 
 //=================Test cases====================
-
 // uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
+// Passed
 // uniteUnique([1, 3, 2], [1, [5]], [2, [4]]) should return [1, 3, 2, [5], [4]].
+// Passed
 // uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
+// Passed
 // uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
