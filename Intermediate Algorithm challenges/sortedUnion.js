29ad412fe0c21fdfd4f85e6499a835407faa3787
Jatin Panjwani <jatinpanjwani12@gmail.com>
2018-12-25 18:51:06 +0530

    Test cases added

--- Intermediate Algorithm challenges/sortedUnion.js
+++ Intermediate Algorithm challenges/sortedUnion.js
@@ -26,3 +26,10 @@ function removeDuplicates(src,consistsDups){
 
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+//=================Test cases====================
+
+// uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
+// uniteUnique([1, 3, 2], [1, [5]], [2, [4]]) should return [1, 3, 2, [5], [4]].
+// uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
+// uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
