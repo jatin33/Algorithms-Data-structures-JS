e0dee5a03b91a6d4aa082d636d6741247605757f
Jatin Panjwani <jatinpanjwani12@gmail.com>
2018-12-26 19:34:32 +0530

    All test cases passed

--- Intermediate Algorithm challenges/sumAllOddFibonacciNumbers.js
+++ Intermediate Algorithm challenges/sumAllOddFibonacciNumbers.js
@@ -26,3 +26,17 @@ function generateFibonacci(n){
 function isOdd(num){
   return num % 2 == 0 ? false : true;
 }
+
+//===========Test Cases==============
+// sumFibs(1) should return a number. 
+// Passed
+// sumFibs(1000) should return 1785.
+// Passed
+// sumFibs(4000000) should return 4613732.
+// Passed
+// sumFibs(4) should return 5.
+// Passed
+// sumFibs(75024) should return 60696.
+// Passed
+// sumFibs(75025) should return 135721
+// Passed
