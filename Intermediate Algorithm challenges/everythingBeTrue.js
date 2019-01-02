2c8ad89f28b34388d566ff3e6ebfe566b51b8e48
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-02 09:18:07 +0530

    All test cases passed

--- Intermediate Algorithm challenges/everythingBeTrue.js
+++ Intermediate Algorithm challenges/everythingBeTrue.js
@@ -27,3 +27,23 @@ function truthCheck(collection, pre) {
 
 
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+
+//===========================Test Cases========================
+// truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") should return true.
+// Passed
+// truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") should return false.
+// Passed
+// truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age") should return false.
+// Passed
+// truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat") should return false
+// Passed
+// truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastFoward", "onBoat": true}], "onBoat") should return true
+// Passed
+// truthCheck([{"single": "yes"}], "single") should return true
+// Passed
+// truthCheck([{"single": ""}, {"single": "double"}], "single") should return false
+// Passed
+// truthCheck([{"single": "double"}, {"single": undefined}], "single") should return false
+// Passed
+// truthCheck([{"single": "double"}, {"single": NaN}], "single") should return false
+// Passed
