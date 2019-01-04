cbcf9ae5dfb6ad56cf5a8e1fef7918817d785aab
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-04 23:13:49 +0530

    All test cases passed

--- Intermediate Algorithm challenges/mapTheDebris.js
+++ Intermediate Algorithm challenges/mapTheDebris.js
@@ -17,3 +17,9 @@ function orbitalPeriod(arr) {
 }
 
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+
+// ============ Test Cases =============
+Passed
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]) should return [{name: "sputnik", orbitalPeriod: 86400}].
+Passed
+orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]) should return [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}]
