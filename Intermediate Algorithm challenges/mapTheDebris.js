9f6898841ce583b2b2ea54b6f262897825bfac38
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-04 22:38:56 +0530

    Create  mapTheDebris.js

---
+++ Intermediate Algorithm challenges/mapTheDebris.js
@@ -0,0 +1,15 @@
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  let results = [];
+  let period = 0;
+  arr.forEach((ele)=>{
+    let time = 2*Math.PI*Math.sqrt(Math.pow((earthRadius + ele.avgAlt),3)/GM);
+    delete ele.avgAlt;
+    ele.orbitalPeriod = Math.round(time);
+  });
+
+  return arr;
+}
+
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
