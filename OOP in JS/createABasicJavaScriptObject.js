b8551c02bba83eeec6c059b2f944aa204dd40b54
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-05 00:25:09 +0530

    Create createABasicJavaScriptObject.js

---
+++ OOP in JS/createABasicJavaScriptObject.js
@@ -0,0 +1,9 @@
+//Problem statement:Think about things people see everyday, like cars, shops, and birds. These are all objects: tangible things people can observe and interact with.
+// What are some qualities of these objects? A car has wheels. Shops sell items. Birds have wings.
+// These qualities, or properties, define what makes up an object. Note that similar objects share the same properties, but may have different values for those properties. For example, all cars have wheels, but not all cars have the same number of wheels.
+
+
+let dog = {
+ name : '',
+ numLegs : 2  
+};
