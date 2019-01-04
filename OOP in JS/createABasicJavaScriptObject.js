88227f35570f134c5603e91082e0dd20a215fa58
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-05 00:40:53 +0530

    Different methods to create object listed

--- OOP in JS/createABasicJavaScriptObject.js
+++ OOP in JS/createABasicJavaScriptObject.js
@@ -2,8 +2,14 @@
 // What are some qualities of these objects? A car has wheels. Shops sell items. Birds have wings.
 // These qualities, or properties, define what makes up an object. Note that similar objects share the same properties, but may have different values for those properties. For example, all cars have wheels, but not all cars have the same number of wheels.
 
-
+//Object Literal notation
 let dog = {
  name : '',
  numLegs : 2  
 };
+
+// Using function constructor
+
+// Using Object.create() method
+
+// using ES6 class
