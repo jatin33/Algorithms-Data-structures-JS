7ec370cd893503100e35261c9fa67082451e7373
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-07 03:09:12 +0530

    Additional info added on constructor property

--- OOP in JS/setConstructorProperty.js
+++ OOP in JS/setConstructorProperty.js
@@ -35,3 +35,8 @@ Dog.prototype = {
     console.log("My name is " + this.name); 
   }
 };
+
+// Note:
+// The constructor property returns a reference to the Object constructor function that created the instance object. 
+// Note that the value of this property is a reference to the function itself, not a string containing the function's name.
+// The value is only read-only for primitive values such as 1, true and "test".
