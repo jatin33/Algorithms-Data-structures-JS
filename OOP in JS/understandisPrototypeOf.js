d25ca8dacc079557caf7714dc8d7d74e835c6e4d
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-07 03:01:24 +0530

    Some doubts updated

--- OOP in JS/understandisPrototypeOf.js
+++ OOP in JS/understandisPrototypeOf.js
@@ -1,14 +1,29 @@
-Understand Where an Object’s Prototype Comes From
-Just like people inherit genes from their parents, an object inherits its prototype directly from the constructor function that created it. For example, here the Bird constructor creates the duck object:
+// Understand Where an Object’s Prototype Comes From
+// Just like people inherit genes from their parents, an object inherits its prototype directly from the constructor function that created it. For example, here the Bird constructor creates the duck object:
 
-function Bird(name) {
+// function Bird(name) {
+//   this.name = name;
+// }
+
+// let duck = new Bird("Donald");
+// duck inherits its prototype from the Bird constructor function. You can show this relationship with the isPrototypeOf method:
+
+// Bird.prototype.isPrototypeOf(duck);
+// // returns true
+
+// Task to complete : Use isPrototypeOf to check the prototype of beagle.
+
+function Dog(name) {
   this.name = name;
 }
 
-let duck = new Bird("Donald");
-duck inherits its prototype from the Bird constructor function. You can show this relationship with the isPrototypeOf method:
+let beagle = new Dog("Snoopy");
 
-Bird.prototype.isPrototypeOf(duck);
-// returns true
+// Add your code below this line
 
-Use isPrototypeOf to check the prototype of beagle.
+Dog.prototype.isPrototypeOf(beagle);
+
+// Some questions lingering in my mind
+// ========================================================================================================
+// What is the difference between functionality of instanceof operator and isPrototypeOf() method ?
+// We can also create same result with beagle.constructor = Dog then why should we prefer isPrototypeOf() method ?
