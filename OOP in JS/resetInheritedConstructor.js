9c27f3d89fbdff5d3d850129c17e3f07ffbbee8e
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-07 04:43:20 +0530

    Task completed

--- OOP in JS/resetInheritedConstructor.js
+++ OOP in JS/resetInheritedConstructor.js
@@ -1,15 +1,30 @@
-Reset an Inherited Constructor Property
-When an object inherits its prototype from another object, it also inherits the supertype's constructor property.
+// Reset an Inherited Constructor Property
+// When an object inherits its prototype from another object, it also inherits the supertype's constructor property.
 
-Here's an example:
+// Here's an example:
 
+// function Bird() { }
+// Bird.prototype = Object.create(Animal.prototype);
+// let duck = new Bird();
+// duck.constructor // function Animal(){...}
+// But duck and all instances of Bird should show that they were constructed by Bird and not Animal. To do so, you can manually set Bird's constructor property to the Bird object:
+
+// Bird.prototype.constructor = Bird;
+// duck.constructor // function Bird(){...}
+
+// Task to complete : Fix the code so duck.constructor and beagle.constructor return their respective constructors.
+// ===============================================================================================
+function Animal() { }
 function Bird() { }
+function Dog() { }
+
 Bird.prototype = Object.create(Animal.prototype);
-let duck = new Bird();
-duck.constructor // function Animal(){...}
-But duck and all instances of Bird should show that they were constructed by Bird and not Animal. To do so, you can manually set Bird's constructor property to the Bird object:
+Dog.prototype = Object.create(Animal.prototype);
 
+// Add your code below this line
 Bird.prototype.constructor = Bird;
-duck.constructor // function Bird(){...}
+Dog.prototype.constructor = Dog;
 
-Fix the code so duck.constructor and beagle.constructor return their respective constructors.
+
+let duck = new Bird();
+let beagle = new Dog();
