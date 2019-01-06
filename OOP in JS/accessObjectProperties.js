04a07fe353523441d16d521de60c6ef0b7f2b883
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-06 17:48:04 +0530

    bracket notation added in js

--- OOP in JS/accessObjectProperties.js
+++ OOP in JS/accessObjectProperties.js
@@ -1,3 +1,5 @@
+// Dot notation
+
 let dog = {
   name: "Spot",
   numLegs: 4
@@ -5,3 +7,14 @@ let dog = {
 // Add your code below this line
 console.log(dog.name);
 console.log(dog.numLegs);
+
+// Bracket notation
+
+let dog = {
+  name: "Spot",
+  numLegs: 4
+};
+// Add your code below this line
+console.log(dog['name']);
+console.log(dog['numLegs']);
+// P.S -> keys are always string in javascript.(Property(key:value))
