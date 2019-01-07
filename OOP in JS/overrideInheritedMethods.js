d3b75f210d8e8e235ef15dea34933d59d253e913
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-07 05:51:19 +0530

    Task completed

--- OOP in JS/overrideInheritedMethods.js
+++ OOP in JS/overrideInheritedMethods.js
@@ -1,36 +1,54 @@
-Override Inherited Methods
-In previous lessons, you learned that an object can inherit its behavior (methods) from another object by cloning its prototype object:
+// Override Inherited Methods
+// In previous lessons, you learned that an object can inherit its behavior (methods) from another object by cloning its prototype object:
 
-ChildObject.prototype = Object.create(ParentObject.prototype);
-Then the ChildObject received its own methods by chaining them onto its prototype:
+// ChildObject.prototype = Object.create(ParentObject.prototype);
+// Then the ChildObject received its own methods by chaining them onto its prototype:
 
-ChildObject.prototype.methodName = function() {...};
-It's possible to override an inherited method. It's done the same way - by adding a method to ChildObject.prototype using the same method name as the one to override.
+// ChildObject.prototype.methodName = function() {...};
+// It's possible to override an inherited method. It's done the same way - by adding a method to ChildObject.prototype using the same method name as the one to override.
 
-Here's an example of Bird overriding the eat() method inherited from Animal:
+// Here's an example of Bird overriding the eat() method inherited from Animal:
 
-function Animal() { }
-Animal.prototype.eat = function() {
-  return "nom nom nom";
-};
+// function Animal() { }
+// Animal.prototype.eat = function() {
+//   return "nom nom nom";
+// };
+// function Bird() { }
+
+// // Inherit all methods from Animal
+// Bird.prototype = Object.create(Animal.prototype);
+
+// // Bird.eat() overrides Animal.eat()
+// Bird.prototype.eat = function() {
+//   return "peck peck peck";
+// };
+// If you have an instance let duck = new Bird(); and you call duck.eat(), this is how JavaScript looks for the method on duck’s prototype chain:
+
+// 1. duck => Is eat() defined here? No.
+
+// 2. Bird => Is eat() defined here? => Yes. Execute it and stop searching.
+
+// 3. Animal => eat() is also defined, but JavaScript stopped searching before reaching this level.
+
+// 4. Object => JavaScript stopped searching before reaching this level.
+
+// ==================================================================================
+// Task to complete : Override the fly() method for Penguin so that it returns "Alas, this is a flightless bird."
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 function Bird() { }
 
-// Inherit all methods from Animal
-Bird.prototype = Object.create(Animal.prototype);
+Bird.prototype.fly = function() { return "I am flying!"; };
 
-// Bird.eat() overrides Animal.eat()
-Bird.prototype.eat = function() {
-  return "peck peck peck";
-};
-If you have an instance let duck = new Bird(); and you call duck.eat(), this is how JavaScript looks for the method on duck’s prototype chain:
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
 
-1. duck => Is eat() defined here? No.
+// Add your code below this line
+Penguin.prototype.fly = function() {
+    return 'Alas, this is a flightless bird.';
+}
+// Just reassign different function object to same fly property on prototype chain
+// Add your code above this line
 
-2. Bird => Is eat() defined here? => Yes. Execute it and stop searching.
-
-3. Animal => eat() is also defined, but JavaScript stopped searching before reaching this level.
-
-4. Object => JavaScript stopped searching before reaching this level.
-
-
-Override the fly() method for Penguin so that it returns "Alas, this is a flightless bird."
+let penguin = new Penguin();
+console.log(penguin.fly());
