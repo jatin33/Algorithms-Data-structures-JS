adc2ecb4559dc5a2125992febef54aec4092eced
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-10 20:27:55 +0530

    Create usingMap.js

---
+++ Functional Programming in JS/usingMap.js
@@ -0,0 +1,5 @@
+Task defined
+=============================================
+The watchList array holds objects with information on several movies. Use map to pull the title 
+and rating from watchList and save the new array in the rating variable. 
+The code in the editor currently uses a for loop to do this, replace the loop functionality with your map expression.
