deeeb2f4e4b2dfa3cb505864ce0d20332816d9d6
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-13 00:49:39 +0530

    Problem defined

--- Functional Programming in JS/usingReduce.js
+++ Functional Programming in JS/usingReduce.js
@@ -1,3 +1,11 @@
+// Problem defined :
+// The variable watchList holds an array of objects with information on several movies. 
+// Use reduce to find the average IMDB rating of the movies directed by Christopher Nolan.
+// Recall from prior challenges how to filter data and map over it to pull what you need.
+// You may need to create other variables, but save the final average into the variable averageRating.
+// Note that the rating values are saved as strings in the object and need 
+// to be converted into numbers before they are used in any mathematical operations.
+
 // the global variable
 var watchList = [
                  {  
