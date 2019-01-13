cd071f796e945868578c4bd2490fe0b7f69ed1f1
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-13 19:56:45 +0530

    All test cases completed

--- Functional Programming in JS/creatingURLslugs.js
+++ Functional Programming in JS/creatingURLslugs.js
@@ -1,3 +1,13 @@
+// Problem statement:
+// Fill in the urlSlug function so it converts a string title and returns the hyphenated version for the URL. 
+// You can use any of the methods covered in this section, and don't use replace. 
+// Here are the requirements:
+// The input is a string with spaces and title-cased words
+// The output is a string with the spaces between words replaced by a hyphen (-)
+// The output should be all lower-cased letters
+// The output should not have any spaces
+
+
 // the global variable
 var globalTitle = "Winter Is Coming";
 
@@ -8,3 +18,16 @@ function urlSlug(title) {
 // Add your code above this line
 
 var winterComing = urlSlug(globalTitle); // Should be "winter-is-coming"
+
+// // =================Test Cases===============
+// The globalTitle variable should not change.
+// Passed
+// Your code should not use the replace method for this challenge.
+// Passed
+// urlSlug("Winter Is Coming") should return "winter-is-coming".
+// Passed
+// urlSlug(" Winter Is  Coming") should return "winter-is-coming".
+// Passed
+// urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone") should return "a-mind-needs-books-like-a-sword-needs-a-whetstone".
+// Passed
+// urlSlug("Hold The Door") should return "hold-the-door"
