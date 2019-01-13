631fe0c48be97293624c1c5380f4cc7d5218b3f7
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-13 19:08:50 +0530

    Problem statement updated

--- Functional Programming in JS/usingSplit.js
+++ Functional Programming in JS/usingSplit.js
@@ -1,3 +1,7 @@
+// Problem statement: Use the split method inside the splitify function to split str into an array of words. 
+// The function should return the array. 
+// Note that the words are not always separated by spaces, and the array should not contain punctuation.
+
 function splitify(str) {
   // Add your code below this line
   let re = /[-|,|\s|.]/g;
@@ -5,3 +9,4 @@ function splitify(str) {
   // Add your code above this line
 }
 splitify("Hello World,I-am code");
+
