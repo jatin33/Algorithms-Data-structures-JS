0f9372f98cbbfe40a7325938989b269e6703f60c
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-13 19:18:43 +0530

    All test cases passed

--- Functional Programming in JS/usingJoin.js
+++ Functional Programming in JS/usingJoin.js
@@ -1,3 +1,10 @@
+// Problem statement : 
+// Use the join method (among others) inside the sentensify function to make a sentence from the words in the string str. 
+// The function should return a string.
+// For example, "I-like-Star-Wars" would be converted to "I like Star Wars". 
+// For this challenge, do not use the replace method.
+// ================================
+
 function sentensify(str) {
   // Add your code below this line
   let re = /[-|,|.]/g;
@@ -5,3 +12,17 @@ function sentensify(str) {
   // Add your code above this line
 }
 sentensify("May-the-force-be-with-you");
+
+// ==============Test Cases===========
+// Your code should use the join method.
+// Passed
+// Your code should not use the replace method.
+// Passed
+// sentensify("May-the-force-be-with-you") should return a string.
+// Passed
+// sentensify("May-the-force-be-with-you") should return "May the force be with you".
+// Passed
+// sentensify("The.force.is.strong.with.this.one") should return "The force is strong with this one".
+// Passed
+// sentensify("There,has,been,an,awakening") should return "There has been an awakening".
+// Passed
