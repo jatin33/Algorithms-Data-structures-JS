5b0102a7b2200b837f3471dac069d981e8e4930b
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-15 10:07:00 +0530

    Task completed

--- ES6/higherOrderArrowFunc.js
+++ ES6/higherOrderArrowFunc.js
@@ -1,8 +1,20 @@
-Higher order functions :- Function taking another function as its argument
+// Higher order functions :- Function taking another function as its argument
 
-========================
-Task defined:
-Use arrow function syntax to compute the square of only the positive integers 
-(decimal numbers are not integers) in the array realNumberArray and store the new array in the variable squaredIntegers
-========================
+// ========================
+// Task defined:
+// Use arrow function syntax to compute the square of only the positive integers 
+// (decimal numbers are not integers) in the array realNumberArray and store the new array in the variable squaredIntegers
+// ========================
 
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+  "use strict";
+  // change code below this line
+  const squaredIntegers = arr.filter((num)=> num > 0 && Number.isInteger(num)).map((num)=> num*num);
+  // change code above this line
+
+  return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
