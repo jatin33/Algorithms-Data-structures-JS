4a0ed67a95f95669cf4d01713ae0ca2d37432fae
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-15 10:06:13 +0530

    Task defined

---
+++ ES6/higherOrderArrowFunc.js
@@ -0,0 +1,8 @@
+Higher order functions :- Function taking another function as its argument
+
+========================
+Task defined:
+Use arrow function syntax to compute the square of only the positive integers 
+(decimal numbers are not integers) in the array realNumberArray and store the new array in the variable squaredIntegers
+========================
+
