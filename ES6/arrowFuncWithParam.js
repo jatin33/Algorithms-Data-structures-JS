209bd0c1be7ca9102abe06db5f268fd430928457
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-15 09:57:10 +0530

    Task completed

--- ES6/arrowFuncWithParam.js
+++ ES6/arrowFuncWithParam.js
@@ -1,10 +1,13 @@
-Just like a normal function, you can pass arguments into arrow functions.
+// Just like a normal function, you can pass arguments into arrow functions.
 
-// doubles input value and returns it
-const doubler = (item) => item * 2;
-You can pass more than one argument into arrow functions as well.
+// // doubles input value and returns it
+// const doubler = (item) => item * 2;
+// You can pass more than one argument into arrow functions as well.
 
-===========================
-Task defined
-Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax.
-===========================
+// ===========================
+// Task defined
+// Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax.
+// ===========================
+const myConcat = (arr1, arr2) =>  arr1.concat(arr2);
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
