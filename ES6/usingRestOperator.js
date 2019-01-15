f26ed7e5813a31434a21e4ea175feb817836ea40
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-15 10:27:43 +0530

    Create usingRestOperator.js

---
+++ ES6/usingRestOperator.js
@@ -0,0 +1,12 @@
+What is rest operator?
+... is the rest operator(3 dots)
+
+Why it is used?
+ With the rest operator, you can create functions that take a variable number of arguments. 
+ These arguments are stored in an array like object that can be accessed later from inside the function.
+ It is an array like object. Therefore performing some operations on “arguments” will give an error.
+ 
+ =================
+ Task defined :
+ Modify the function sum so that it uses the rest operator and it works in the same way with any number of parameters.
+ ================
