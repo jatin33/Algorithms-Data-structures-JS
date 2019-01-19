b3c33a83346692be9a6a8413ab0aa17bc129f037
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-19 12:06:37 +0530

    Task defined

--- Regular Expressions/usingTestMethod.js
+++ Regular Expressions/usingTestMethod.js
@@ -1,1 +1,15 @@
+// What does test() function does?
+// One way to test a regex is using the .test() method. The .test() method takes the regex,
+// applies it to a string (which is placed inside the parentheses), 
+// and returns true or false if your pattern finds something or not.
 
+// What does it take as argument and what does it output as result?
+// in-args => string on which pattern matching takes place
+// output => Boolean(true/false)
+
+// =============Task Defined==============
+// Apply the regex myRegex on the string myString using the .test() method.
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result =myRegex.test(myString); // Change this line
