2a490c91eb55db6dd4dbda91eded5c3c3e540ad9
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-01-22 10:46:15 +0530

    Task completed

--- Regular Expressions/checkForAllOrNone.js
+++ Regular Expressions/checkForAllOrNone.js
@@ -1,15 +1,29 @@
-Sometimes the patterns you want to search for may have parts of it that may or may not exist. 
-However, it may be important to check for them nonetheless.
+// Sometimes the patterns you want to search for may have parts of it that may or may not exist. 
+// However, it may be important to check for them nonetheless.
 
-You can specify the possible existence of an element with a question mark, ?.
-This checks for zero or one of the preceding element. You can think of this symbol as saying the previous element is optional.
+// You can specify the possible existence of an element with a question mark, ?.
+// This checks for zero or one of the preceding element. You can think of this symbol as saying the previous element is optional.
 
-For example, there are slight differences in American and British English and you can use the question mark to match both spellings.
+// For example, there are slight differences in American and British English and you can use the question mark to match both spellings.
 
-let american = "color";
-let british = "colour";
-let rainbowRegex= /colou?r/;
-rainbowRegex.test(american); // Returns true
-rainbowRegex.test(british); // Returns true
+// let american = "color";
+// let british = "colour";
+// let rainbowRegex= /colou?r/;
+// rainbowRegex.test(american); // Returns true
+// rainbowRegex.test(british); // Returns true
 
-Change the regex favRegex to match both the American English (favorite) and the British English (favourite) version of the word.
+// ==========Task defined===========
+// Change the regex favRegex to match both the American English (favorite) and the British English (favourite) version of the word.
+let favWord = "favorite";
+let favRegex = /favou?rite/; // Change this line
+let result = favRegex.test(favWord);
+
+// =========Test cases=========
+// Your regex should use the optional symbol, ?.
+// Passed
+// Your regex should match "favorite"
+// Passed
+// Your regex should match "favourite"
+// Passed
+// Your regex should not match "fav"
+// Passed
