1fa516d2a53cbec1d894975ed6967ec17d32582a
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-03-23 18:09:31 +0530

    Project finished with all test cases

--- Data Structure And Algorithm Projects/Roman Numeral Convertor/index.js
+++ Data Structure And Algorithm Projects/Roman Numeral Convertor/index.js
@@ -1,9 +1,27 @@
 const romanToDecimal = {
-'I' : 1,
-'V' : 5,
-'X' : 10,
-'L' : 50,
-'C' : 100,
-'D' : 500,
-'M' : 1000
+1 : 'I',
+5 : 'V',
+10 : 'X',
+50 : 'L',
+100 : 'C',
+500 : 'D',
+1000 : 'M'
 };
+
+function convertToRoman(num) {
+    let romanString = '';
+    let digits = [];
+    while(num > 0){
+        digits.shift(num % 10);
+        num = Math.floor(num / 10);
+    }
+    console.log(digits);
+    // return num;
+   }
+
+function generateRomanNumeral(num){
+    if(num === 1){
+        return romanToDecimal[1];
+    }
+}   
+
