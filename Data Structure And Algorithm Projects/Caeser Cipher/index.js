2f02beb8de4861d293b0b13a4df5f53084cbbe86
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-03-25 00:30:58 +0530

    Project finished

--- Data Structure And Algorithm Projects/Caeser Cipher/index.js
+++ Data Structure And Algorithm Projects/Caeser Cipher/index.js
@@ -1,2 +1,20 @@
-console.log('new commit');
-
+function rot13(str) { // LBH QVQ VG!
+    let decodedStr = '';
+    for(let i = 0; i < str.length; i++){
+      if(str.charAt(i) === ' ' || str.charAt(i) === '?'|| str.charAt(i) === '!'|| str.charAt(i) === '.'){
+        decodedStr += str.charAt(i);
+     } else{
+      decodedStr += decipheredAlphabet(str.charAt(i));
+     }
+    } 
+    return decodedStr;
+  }
+  
+  // Change the inputs below to test
+  rot13("SERR PBQR PNZC?!.");
+  
+  function decipheredAlphabet(str) {
+    const alphaIndex = str.charCodeAt(0) - 65 - 13; 
+    const transformedAlphaCode = (alphaIndex % 26 + 26) % 26;
+    return String.fromCharCode(transformedAlphaCode + 65);
+  };
